bb3f39b5a61a4a5a068cafef4f3463a9db38143f
sandenson <lgabrielsandes@gmail.com>
2023-10-12 12:05:09 -0300

    feat: Class 2 "Database Integration" finished.

--- todolist/src/main/java/com/sandenson/todolist/users/UserController.java
+++ todolist/src/main/java/com/sandenson/todolist/users/UserController.java
@@ -1,5 +1,7 @@
 package com.sandenson.todolist.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping
-    public void create(@RequestBody UserModel model) {
-        System.out.println("nyahello, " + model.name);
+    public ResponseEntity create(@RequestBody UserModel model) {
+        var user = this.userRepository.findByUsername(model.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.badRequest().body("User already exists");
+        }
+
+        var createdUser = this.userRepository.save(model);
+        return ResponseEntity.ok(createdUser);
     }
 }
